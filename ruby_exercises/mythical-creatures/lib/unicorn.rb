46d7b8947513cfb54e14bef5dd1fedad503045f0
Parker Boeing <parker.boeing@gmail.com>
2023-05-09 07:11:17 -0600

    Add a Unicorn class

---
+++ ruby_exercises/mythical-creatures/lib/unicorn.rb
@@ -0,0 +1,18 @@
+class Unicorn
+    attr_reader :name, :color
+    
+    def initialize(name, color = "silver")
+        @name = name
+        @color = color
+    end
+
+    def silver?
+        return true if color == "silver"
+        false
+    end
+    
+    def say(stuff)
+        "**;* #{stuff} **;*"
+    end
+end
+
