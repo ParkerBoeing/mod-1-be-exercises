8607d961f1069ae574a815d33f966b7631883ced
Parker Boeing <parker.boeing@gmail.com>
2023-05-17 12:04:02 -0600

    Feat/Test: created venue class and tests for methods

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,31 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron_name)
+    @patrons << patron_name
+  end
+
+  def yell_at_patrons
+    patrons.map { |patron_name| patron_name.upcase}
+  end
+
+  def over_capacity?
+    if patrons.count <= @capacity
+      return false
+    else
+      return true
+    end
+  end
+
+  def kick_out
+    while over_capacity?
+      patrons.pop
+    end
+  end
 end
